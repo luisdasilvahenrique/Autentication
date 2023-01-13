89d86727f5afd878574e887363d0edbe863d0222
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2023-01-12 23:23:41 -0300

    Salvando tokens nos cookies

--- frontend/context/AuthContext.tsx
+++ frontend/context/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, ReactNode, useState } from "react";
 import Router from "next/router";
 import { api } from "../service/api";
+import { setCookie } from 'nookies'
 
 type User = {
     email: string;
@@ -37,6 +38,15 @@ export function AuthProvider({ children }: AuthProviderProps) {
             })
             
             const { token, refreshToken ,permissions, roles } = response.data;
+
+            setCookie(undefined, 'nextauth.token', token, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/'
+            })
+            setCookie(undefined, 'nextauth.refreshToken', refreshToken , {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/'
+            })
 
             setUser({
                 email,
